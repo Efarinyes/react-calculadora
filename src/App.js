1eee019ad408a77a4f338f8c3c3ec4252aae3e2b
Efarinyes <efarinyes@gmail.com>
2022-02-15 16:05:00 +0100

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+function Button({ onPressed, children }) {
+  return <button onClick={onPressed}> {children}</button>;
+}
+
+const initialAntic = 0;
+const initialDisplay = 0;
+const initialOperacio = "";
 function App() {
+  const [antic, setAntic] = useState(initialAntic);
+  const [operacio, setOperacio] = useState(initialOperacio);
+  const [novaEntrada, setNovaEntrada] = useState(false);
+  const [display, setDisplay] = useState(initialDisplay);
+
+  const entrarNumero = (numero) => {
+    if (novaEntrada) setDisplay(numero);
+    else setDisplay(display * 10 + numero);
+    setNovaEntrada(false);
+  };
+
+  const entraOperacio = (operacio) => {
+    setNovaEntrada(true);
+    setAntic(display);
+    if (!novaEntrada) calcular();
+    setOperacio(operacio);
+  };
+  const calcular = () => {
+    if (operacio === "+") setDisplay(antic + display);
+    if (operacio === "-") setDisplay(antic - display);
+  };
+
+  const clear = () => {
+    setOperacio("");
+    setDisplay(initialDisplay);
+    setAntic(initialAntic);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculadora">
+      <div className="display">{display}</div>
+      <div className="numeros">
+        {numeros.map((numero) => (
+          <Button onPressed={() => entrarNumero(numero)} key={numero}>
+            {numero}
+          </Button>
+        ))}
+      </div>
+      <div className="botons">
+        <button onClick={() => entraOperacio("+")}>+</button>
+        <button onClick={calcular}>=</button>
+        <button onClick={() => entraOperacio("-")}>-</button>
+        <button onClick={clear}>C</button>
+      </div>
     </div>
   );
 }
